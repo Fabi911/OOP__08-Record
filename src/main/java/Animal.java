694f6758f31f989ff3c4c5024c7a7958a51e3942
Fabian Döz <fabian@doez.info>
2024-07-09 11:50:06 +0200

    upload

---
+++ src/main/java/Animal.java
@@ -0,0 +1,4 @@
+public record Animal(
+        String anmialID, String name, String species, int age
+) {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal lion =new Animal("12312313","Lion", "predator",5);
+        Animal monkey =new Animal("12312315","Monkey", "monkey",3);
+        Animal deer = new Animal("12312316","Deer", "deer",2);
+        System.out.println(lion);
+        System.out.println(monkey);
+        System.out.println(deer);
+    }
+}
